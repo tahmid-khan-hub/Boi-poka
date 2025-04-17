2a452f93888b72da3d0405fa006ad69b9fb5859f
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-04-18 00:02:38 +0600

    added book id in local storage

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import { addToStoredDB } from '../../utility/addToDB';
 
 const BookDetails = () => {
 
@@ -16,13 +17,17 @@ const BookDetails = () => {
 
     const {bookName, image} = singleBook;
 
-    
+    const handleMarkAsRead = (id) =>{
+
+        addToStoredDB(id);
+        
+    }
 
     return (
         <div className=''>
             <h3 className='text-2xl mx-auto'>{bookName}</h3>
             <img className='w-48' src={image} alt="" />
-            <button className='btn m-2'>read</button>
+            <button onClick={()=>handleMarkAsRead(id)} className='btn m-2'>read</button>
             <button className='btn m-2'>wish list</button>
 
         </div>
