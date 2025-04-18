774e904a2c7bae6da2b23f07e003eb11fa0ce410
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-04-18 14:40:54 +0600

    added sort by

--- src/Pages/ReadList/ReadList.jsx
+++ src/Pages/ReadList/ReadList.jsx
@@ -8,6 +8,7 @@ import Book from '../Book/Book';
 const ReadList = () => {
 
     const [readList, setReadList] = useState([])
+    const [sort, setSort] = useState("")
 
     const data = useLoaderData();
     console.log(data);
@@ -19,8 +20,27 @@ const ReadList = () => {
         setReadList(myReadList)
     },[])
 
+    const handleSort = (type) =>{
+        setSort(type);
+        if(type === "pages"){
+            const sortByPage = [...readList].sort((a,b) => a.totalPages - b.totalPages);
+            setReadList(sortByPage);
+        }
+        if(type === "ratings"){
+            const sortByRatings = [...readList].sort((a,b) => a.rating - b.rating);
+            setReadList(sortByRatings);
+        }
+    }
+
     return (
         <div>
+            <details className="dropdown">
+                <summary className="btn m-1">sort By: {sort?sort:""}</summary>
+                <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                    <li><a onClick={()=>handleSort("pages")}>Pages</a></li>
+                    <li><a onClick={()=>handleSort("ratings")}>Ratings</a></li>
+                </ul>
+            </details>
             <Tabs>
                 <TabList>
                     <Tab>Read Book List</Tab>
